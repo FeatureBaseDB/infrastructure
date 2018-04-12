cb01420c14aa8e552199838dd8a4ab57d84b82f5
Cody Soyland <cody@soyland.org>
2018-04-12 08:17:15 -0500

    Refactor tmuxcluster: Split into two functions

--- local/tmuxcluster.sh
+++ local/tmuxcluster.sh
@@ -9,47 +9,70 @@
 # 3. Launch N instances of pilosa, each on a tmux pane.
 # 4. Attach to tmux session
 
-DATADIR=$(mktemp -d)
+function tmux-split() {
+    # Name of tmux session
+    SESSION=$1
+    shift
+    # Array of commands
+    C=("$@")
+    # Number of commands minus one
+    N=$((${#C[@]} - 1))
+
+    tmux new-session -d -s $SESSION
+    tmux select-window -t $SESSION
+    tmux select-layout even-vertical
+
+    for i in $(seq 0 $N); do
+        # Select the "Nth" pane and launch pilosa
+        tmux select-pane -t $i
+        tmux send-keys "${C[i]}" Enter
+
+        # TODO: This splitting logic works up to only about 5 nodes before failing with "create pane failed: pane too small"
+        # Split the window after every loop except the last
+        if [ ! $i -eq $N ]; then
+            tmux split-window -v
+            tmux select-layout even-vertical
+        fi
+    done
+
+    # Uncomment and use `-CC` if using tmux/iterm integration
+    tmux -CC attach -t $SESSION
+    #tmux attach -t $SESSION
+}
+
+function make-cluster() {
+    NUMNODES=${1:-3} # Number of nodes, default 3
+    DATADIR=$(mktemp -d)
+
+    echo "Data directory is ${DATADIR}"
+    echo "Starting ${NUMNODES}-node cluster..."
+
+    # Make seed list for config
+    for i in $(seq 0 $(( $NUMNODES - 1 ))); do
+        if [ -z $SEEDS ]; then
+            SEEDS=localhost:$((14000 + $i))
+        else
+            SEEDS=$SEEDS,localhost:$((14000 + $i))
+        fi
+    done
+
+    # Make list of Pilosa commands to run
+    declare -a COMMANDS
+
+    # Generate commands
+    for i in $(seq 0 $(( $NUMNODES - 1 ))); do
+        # Node 0 will be the coordinator
+        if [ $i -eq 0 ]; then
+            COORD=true
+        else
+            COORD=false
+        fi
+
+        COMMANDS[i]="pilosa server --data-dir=${DATADIR}/node${i} --bind=localhost:$((10101 + $i)) --gossip.port=$((14000 + $i)) --gossip.seeds=${SEEDS} --cluster.coordinator=${COORD}"
+    done
+
+    tmux-split pilosa2 "${COMMANDS[@]}"
+}
+
 NUMNODES=${1:-3}
-
-N=$(( $NUMNODES - 1 ))
-
-echo "Starting ${NUMNODES} node cluster..."
-echo "Creating data directories under ${DATADIR}..."
-
-tmux new-session -d -s pilosa
-tmux select-window -t pilosa
-tmux select-layout even-vertical
-
-# Make seed list
-for i in $(seq 0 $N); do
-    if [ -z $SEEDS ]; then
-        SEEDS=localhost:$((14000 + $i))
-    else
-        SEEDS=$SEEDS,localhost:$((14000 + $i))
-    fi
-done
-
-for i in $(seq 0 $N); do
-    # Node 0 will be the coordinator
-    if [ $i -eq 0 ]; then
-        COORD=true
-    else
-        COORD=false
-    fi
-
-    # Select the "Nth" pane and launch pilosa
-    tmux select-pane -t $i
-    tmux send-keys "pilosa server --data-dir=${DATADIR}/node${i} --bind=localhost:$((10101 + $i)) --gossip.port=$((14000 + $i)) --gossip.seeds=${SEEDS} --cluster.coordinator=${COORD}" Enter
-
-    # Split the window after every loop except the last
-    if [ ! $i -eq $N ]; then
-        tmux split-window -v
-    fi
-done
-
-tmux select-layout even-vertical
-
-# Uncomment and use `-CC` if using tmux/iterm integration
-#tmux -CC attach -t pilosa
-tmux attach -t pilosa
+make-cluster $NUMNODES
