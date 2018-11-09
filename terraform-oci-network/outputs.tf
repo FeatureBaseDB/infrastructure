005313692c2ebbc47ace1a9be5368842318d8cee
Cody Soyland <cody@soyland.org>
2018-11-09 15:01:04 -0600

    Add subnets to base network module

--- terraform-oci-network/outputs.tf
+++ terraform-oci-network/outputs.tf
@@ -4,3 +4,12 @@ output "vcn_ocid" {
 output "route_table_ocid" {
   value = "${oci_core_route_table.PilosaRT.id}"
 }
+output "subnet_ocids_iad0" {
+  value = ["${oci_core_subnet.PilosaSubnetIAD0.*.id}"]
+}
+output "subnet_ocids_iad1" {
+  value = ["${oci_core_subnet.PilosaSubnetIAD1.*.id}"]
+}
+output "subnet_ocids_iad2" {
+  value = ["${oci_core_subnet.PilosaSubnetIAD2.*.id}"]
+}
