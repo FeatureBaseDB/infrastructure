118b104e167291be56db6d061f3fae550f956fb4
Cody Soyland <cody@soyland.org>
2019-02-06 13:59:56 -0600

    Update OCI images

--- terraform/oci/pilosa/vars.tf
+++ terraform/oci/pilosa/vars.tf
@@ -19,8 +19,14 @@ variable "instance_shape" {
 variable "instance_image_ocid" {
   type = "map"
 
+  # Oracle Linux 7.6 (CentOS-based)
+  # From https://docs.cloud.oracle.com/iaas/images/image/7d31cb1d-f31f-450c-95c4-0539776c3dcf/
   default = {
-    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaahmblavdr6elu7gmt3jqtjcu2uitlzieb4jv3hh675sjqwadxv5pa"
+    ca-toronto-1 = "ocid1.image.oc1.ca-toronto-1.aaaaaaaafozx4cw5fgcnptx6ukgdjjfzvjb2365chtzprratabynb573wria"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaagbrvhganmn7awcr7plaaf5vhabmzhx763z5afiitswjwmzh7upna"
+    uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaajwtut4l7fo3cvyraate6erdkyf2wdk5vpk6fp6ycng3dv2y3ymvq"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaawufnve5jxze4xf7orejupw5iq3pms6cuadzjc7klojix6vmk42va"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaadjnj3da72bztpxinmqpih62c2woscbp6l3wjn36by2cvmdhjub6a"
   }
 }
 
